b63767b27208800cd14c47d9cd812dbc363f371e
OSBF-Josip <josip.bubalo1999@gmail.com>
2023-10-25 13:41:33 +0200

    treća

--- Vjezba3/SPAVjezba3/unos_ispis.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef UPIS
-#define UPIS
-#include <vector>
-using namespace std;
-void unos_slobodni(vector<int>&, int);
-void unos_ograniceni(vector<int>&, int, int);
-void ispis(vector<int>&, int);
-#endif
